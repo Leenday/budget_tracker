7f0717fbd480dcc32698a2911d840a75907c7488
Leenday <iammilagre@gmail.com>
2024-02-25 14:20:02 +0600

    add react with view_component gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'homepage#index'
   # root to: 'web/incomes#index'
   #
   # scope module: :web do
